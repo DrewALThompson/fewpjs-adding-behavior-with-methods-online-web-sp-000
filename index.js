15c59d44b15a2d095fa4bb27e3dc309ba3b1ea57
Drew Thompson <drewallynthompson@gmail.com>
2021-01-18 08:16:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+class Cat{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  
+  speak(){
+    return `${this.name} says meow!`
+  }
+}
+
+class Dog{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  
+  speak(){}
+}
+
+class Bird{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+}
